4a28bfbc297c86441807b60186238a62406e7dae
ren.yang <goyour123@gmail.com>
2018-08-10 20:03:17 +0800

    Show controller Base Address Register

--- App/XhciViewer/XhciViewer.c
+++ App/XhciViewer/XhciViewer.c
@@ -20,6 +20,7 @@ typedef struct {
   UINT8  Function;
   UINT16 VendorId;
   UINT16 DeviceId;
+  UINT32 BaseAddress[2];
 } PCI_CONFIG;
 
 typedef struct {
@@ -36,6 +37,16 @@ USB3_CONTROLLER*
 ScanXhc (
 );
 
+VOID
+FrontPage (
+);
+
+// VOID
+// Selector (
+//   UINTN                        Column,
+//   UINTN                        Row
+// );
+
 EFI_STATUS
 EFIAPI
 XhciViewerMain (
@@ -46,29 +57,58 @@ XhciViewerMain (
   EFI_STATUS                   Status;
   USB3_CONTROLLER              *Xhc;
   EFI_INPUT_KEY                Key;
-  UINT8                        Index;
+  UINT8                        Cntlr;
 
   Xhc = ScanXhc ();
-  gST->ConIn->ReadKeyStroke(gST->ConIn, &Key);
+  gST->ConIn->ReadKeyStroke (gST->ConIn, &Key);
 
   while (Key.UnicodeChar != 'Q' && Key.UnicodeChar != 'q') {
-    gST->ConOut->ClearScreen(gST->ConOut);
-    Print (L"Index  Bus  Dev  Func  VendorId  DeviceId\n");
-    for (Index = 0; Index < Xhc->XhcNum; Index++) {
-      Print (L"    %x   %02x   %02x    %02x      %04x      %04x\n",
-        Index, 
-        (Xhc->PciConfigBuf + Index * sizeof(PCI_CONFIG))->Bus, 
-        (Xhc->PciConfigBuf + Index * sizeof(PCI_CONFIG))->Device, 
-        (Xhc->PciConfigBuf + Index * sizeof(PCI_CONFIG))->Function, 
-        (Xhc->PciConfigBuf + Index * sizeof(PCI_CONFIG))->VendorId, 
-        (Xhc->PciConfigBuf + Index * sizeof(PCI_CONFIG))->DeviceId
-        );
-    }
+    gST->ConOut->ClearScreen (gST->ConOut);
+    FrontPage (Xhc);
     Status = WaitForKeyStroke (&Key);
+    if (Key.UnicodeChar == CHAR_CARRIAGE_RETURN) {
+      Cntlr = 0;
+      while (Key.UnicodeChar != 'Q' && Key.UnicodeChar != 'q' && Key.ScanCode != SCAN_ESC) {
+        gST->ConOut->ClearScreen (gST->ConOut);
+        Print (L" Base Address Register: %08x", (Xhc->PciConfigBuf + Cntlr * sizeof(PCI_CONFIG))->BaseAddress[0]);
+        Status = WaitForKeyStroke (&Key);
+      }
+    }
   }
 
-  gST->ConOut->ClearScreen(gST->ConOut);
+  gST->ConOut->ClearScreen (gST->ConOut);
   return EFI_SUCCESS;
+}
+
+// VOID
+// Selector (
+//   UINTN                        Column,
+//   UINTN                        Row
+// )
+// {
+//   gST->ConOut->EnableCursor(gST->ConOut, FALSE);
+//   gST->ConOut->SetCursorPosition(gST->ConOut, Column, Row);
+//   Print(L">");
+//   gST->ConOut->SetCursorPosition(gST->ConOut, Column, Row);
+// }
+
+VOID
+FrontPage (
+  USB3_CONTROLLER              *Xhc
+)
+{
+  UINT8                        Index;
+  Print (L" Index  Bus  Dev  Func  VendorId  DeviceId\n");
+  for (Index = 0; Index < Xhc->XhcNum; Index++) {
+    Print (L"     %x   %02x   %02x    %02x      %04x      %04x\n",
+      Index,
+      (Xhc->PciConfigBuf + Index * sizeof (PCI_CONFIG))->Bus, 
+      (Xhc->PciConfigBuf + Index * sizeof (PCI_CONFIG))->Device, 
+      (Xhc->PciConfigBuf + Index * sizeof (PCI_CONFIG))->Function, 
+      (Xhc->PciConfigBuf + Index * sizeof (PCI_CONFIG))->VendorId, 
+      (Xhc->PciConfigBuf + Index * sizeof (PCI_CONFIG))->DeviceId
+      );
+  }
 }
 
 /**
@@ -111,6 +151,7 @@ ScanXhc (
   UINT8                        Func;
   UINT16                       VendorId, DeviceId;
   UINT8                        BaseClass, SubClass, Interface;
+  UINT32                       Bar0, Bar1;
   USB3_CONTROLLER              *Xhc;
 
   Xhc = AllocateZeroPool (sizeof (USB3_CONTROLLER));
@@ -118,23 +159,33 @@ ScanXhc (
   for (Bus = 0; Bus <= MAX_BUS_NUM; Bus++) {
     for (Dev = 0; Dev <= MAX_DEV_NUM; Dev++) {
       for (Func = 0; Func <= MAX_FUNC_NUM; Func++) {
-        VendorId = PciRead16 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0));
-        DeviceId = PciRead16 (PCI_LIB_ADDRESS (Bus, Dev, Func, 2));
+
+        VendorId = PciRead16 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0x0));
         if (VendorId == 0xffff) {
           continue;
         }
+
         BaseClass = PciRead8 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0xb));
         SubClass  = PciRead8 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0xa));
         Interface = PciRead8 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0x9));
+
         if ((BaseClass == XHCI_BASE_CLASS) && (SubClass == XHCI_SUB_CLASS) && (Interface == XHCI_INTERFACE)) {
           Xhc->XhcNum++;
           Xhc->PciConfigBuf = ReallocatePool ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG), Xhc->XhcNum * sizeof (PCI_CONFIG), Xhc->PciConfigBuf);
-          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->Bus      = (Bus & 0xff);
-          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->Device   = Dev;
-          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->Function = Func;
-          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->VendorId = VendorId;
-          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->DeviceId = DeviceId;
+    
+          DeviceId = PciRead16 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0x2));
+          Bar0     = PciRead32 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0x10));
+          Bar1     = PciRead32 (PCI_LIB_ADDRESS (Bus, Dev, Func, 0x14));
+
+          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->Bus            = (Bus & 0xff);
+          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->Device         = Dev;
+          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->Function       = Func;
+          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->VendorId       = VendorId;
+          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->DeviceId       = DeviceId;
+          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->BaseAddress[0] = Bar0;
+          (Xhc->PciConfigBuf + ((Xhc->XhcNum - 1) * sizeof (PCI_CONFIG)))->BaseAddress[1] = Bar1;
         }
+
       }
     }
   }
