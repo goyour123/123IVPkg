1aed79f9619c044c41c09dfecde327b3a20e8405
ren.yang <goyour123@gmail.com>
2021-01-26 18:43:39 +0800

    Implement WaitOBF and ReadEc function

--- Application/Ec/Ec.c
+++ Application/Ec/Ec.c
@@ -4,6 +4,8 @@
 #include <Library/UefiApplicationEntryPoint.h>
 #include <Library/IoLib.h>
 
+#define TIME_OUT               0xffff
+
 // EC Register
 #define EC_COMMAND_STATUS_PORT 0x66
 #define EC_DATA_PORT           0x62
@@ -32,15 +34,15 @@ typedef union {
 EFI_STATUS
 WaitInputBufferEmpty (
   IN UINT16 EcStatusPort
-)
+  )
 {
   EFI_STATUS Status;
   EC_STATUS  EcStatus;
-  UINT16     TimeOut;
+  UINT32     TimeOut;
 
   Status = EFI_DEVICE_ERROR;
 
-  for (TimeOut = 0; TimeOut < 0xFFFF; TimeOut++) {
+  for (TimeOut = 0; TimeOut < TIME_OUT; TimeOut++) {
     EcStatus.Value = IoRead8 (EcStatusPort);
     if (EcStatus.Field.Ibf == 0) {
       Status = EFI_SUCCESS;
@@ -50,6 +52,69 @@ WaitInputBufferEmpty (
     // Delay
     IoRead8 (0x80);
   }
+
+  return Status;
+}
+
+EFI_STATUS
+WaitOutputBufferFull (
+  IN UINT16 EcStatusPort
+  )
+{
+  EFI_STATUS Status;
+  EC_STATUS  EcStatus;
+  UINT32     TimeOut;
+
+  Status = EFI_DEVICE_ERROR;
+
+  for (TimeOut = 0; TimeOut < TIME_OUT; TimeOut++) {
+    EcStatus.Value = IoRead8 (EcStatusPort);
+    if (EcStatus.Field.Obf == 1) {
+      Status = EFI_SUCCESS;
+      break;
+    }
+
+    // Delay
+    IoRead8 (0x80);
+  }
+
+  return Status;
+}
+
+EFI_STATUS
+ReadEc (
+  IN  UINT8 Addr,
+  OUT UINT8 Data
+  )
+{
+  EFI_STATUS Status;
+
+  // Wait IBF empty
+  Status = WaitInputBufferEmpty (EC_COMMAND_STATUS_PORT);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  // Send read EC command
+  IoWrite8 (EC_COMMAND_STATUS_PORT, READ_EC);
+
+  // Wait IBF empty
+  Status = WaitInputBufferEmpty (EC_COMMAND_STATUS_PORT);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  // Send read EC command address
+  IoWrite8 (EC_COMMAND_STATUS_PORT, Addr);
+
+  // Wait OBF full
+  Status = WaitOutputBufferFull (EC_COMMAND_STATUS_PORT);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  // Read output data
+  Data = IoRead8 (EC_DATA_PORT);
 
   return Status;
 }
